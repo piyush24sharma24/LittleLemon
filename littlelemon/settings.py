e1a20aa5c094adff8db3f728d236ca137085cb69
Piyush <piyush9sharma9@gmail.com>
2025-10-18 15:30:36 +0530

    host

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r-%0eqcja86asp_+*1@3d6t9wm65$7^p*v^m961p@)w7*3ob0n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://little-lemon-ovwf.onrender.com', 'localhost', ]
 
 
 # Application definition
