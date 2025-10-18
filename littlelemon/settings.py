2d02de846447bfeb03e780543a920f372e0e50a4
Piyush <piyush9sharma9@gmail.com>
2025-10-18 22:45:13 +0530

    Added Cloudinary for media storage

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
 
 # The settings for media files have been updated for the Graded assessment
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
